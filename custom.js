316d0e470cd7cdb84a73d154aa0c3fb1b818c7f5
Yejoon kim <devc143c@gmail.com>
2022-12-08 13:58:20 +0900

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Custom = () => {
+return (
+	<div
+	style={{
+		display: 'flex',
+		justifyContent: 'Right',
+		alignItems: 'Right',
+		height: '100vh'
+	}}
+	>
+	<h1>커스텀</h1>
+	</div>
+);
+};
+
+export default Custom;
+
---
+++ funding.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Funding = () => {
+return (
+	<div
+	style={{
+		display: 'flex',
+		justifyContent: 'Right',
+		alignItems: 'Right',
+		height: '100vh'
+	}}
+	>
+	<h1>펀딩</h1>
+	</div>
+);
+};
+
+export default Funding;
+
