958793b77e8b0b38c847a756c34d49e7cf01f9b2
hanminJ <wjdgksals56@gmail.com>
2022-12-21 17:15:18 +0900

    update : 카드컴포넌트 사진 크기 변경함

--- client/src/components/UI/ProductCard.jsx
+++ client/src/components/UI/ProductCard.jsx
@@ -26,7 +26,7 @@ toast.success('상품이 장바구니에 들어갔습니다');
        <Col lg='3' md='4' className="mb-2">
         <div className="product__item">
             <div className="product__img">
-                <motion.img whileHover={{scale: 0.9}} src={item.image} alt = " "/>
+                <motion.img whileHover={{scale: 0.9}} src={item.image} style={{height : 200 }} alt = " "/>
             </div>
            <div className="p-2 product__info">
             {/*아래는 상품을 카드 형태로 소개*/}
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,94 +1,94 @@
-import React,{useState} from "react";
-import Helmet from "../components/Helmet/Helmet";
-import { Container,Row,Col,Form,FormGroup } from "reactstrap";
-import {Link} from "react-router-dom";
-import {createUserWithEmailAndPassword,updateProfile } from "firebase/auth";
-import{ref, uploadBytesResumable, getDownloadURL} from "firebase/storage";
-import {setDoc,doc} from "firebase/firestore";
-import {auth} from "../firebase.config";
+// import React,{useState} from "react";
+// import Helmet from "../components/Helmet/Helmet";
+// import { Container,Row,Col,Form,FormGroup } from "reactstrap";
+// import {Link} from "react-router-dom";
+// import {createUserWithEmailAndPassword,updateProfile } from "firebase/auth";
+// import{ref, uploadBytesResumable, getDownloadURL} from "firebase/storage";
+// import {setDoc,doc} from "firebase/firestore";
+// import {auth} from "../firebase.config";
 
 
-import {storage} from "../firebase.config";
-import {db} from "../firebase.config";
+// import {storage} from "../firebase.config";
+// import {db} from "../firebase.config";
 
-import {toast} from "react-toastify";
+// import {toast} from "react-toastify";
 
-import "../styles/login.css";
-import{useNavigate} from "react-router-dom";
+// import "../styles/login.css";
+// import{useNavigate} from "react-router-dom";
 
-const Signup= () => {
+// const Signup= () => {
 
-  const [username,setUsername] = useState("");
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
-  const [file,setFile] = useState(null);
-  const [loading,setLoading] = useState(false);
+//   const [username,setUsername] = useState("");
+//   const [email,setEmail] = useState("");
+//   const [password,setPassword] = useState("");
+//   const [file,setFile] = useState(null);
+//   const [loading,setLoading] = useState(false);
 
-  const navigate = useNavigate()
+//   const navigate = useNavigate()
 
-  const Signup = async(e)=>{
-    e.preventDefault()
-    setLoading(true)
+//   const Signup = async(e)=>{
+//     e.preventDefault()
+//     setLoading(true)
     
-    try {
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-        );
+//     try {
+//       const userCredential = await createUserWithEmailAndPassword(
+//         auth,
+//         email,
+//         password
+//         );
 
         
-        const user= userCredential.user;
+//         const user= userCredential.user;
         
-        const storageRef= ref(storage,`images/${Date.now() + username}`)
-        const uploadTask = uploadBytesResumable(storageRef, file)
+//         const storageRef= ref(storage,`images/${Date.now() + username}`)
+//         const uploadTask = uploadBytesResumable(storageRef, file)
      
-        uploadTask.on((error)=>{
-          toast.error(error.message)
-        },()=>{
-          getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL)=>{
-            //update user profile
-            await updateProfile(user,{
-                displayName: username,
-                photoURL: downloadURL,
+//         uploadTask.on((error)=>{
+//           toast.error(error.message)
+//         },()=>{
+//           getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL)=>{
+//             //update user profile
+//             await updateProfile(user,{
+//                 displayName: username,
+//                 photoURL: downloadURL,
 
-            });
-              //store user data in firestore database
-              await setDoc(doc(db,"users",user.uid),{
-                uid: user.uid,
-                displayName: username,
-                email,
-                photoURL: downloadURL,
-              });
+//             });
+//               //store user data in firestore database
+//               await setDoc(doc(db,"users",user.uid),{
+//                 uid: user.uid,
+//                 displayName: username,
+//                 email,
+//                 photoURL: downloadURL,
+//               });
 
-          });
-        }
-        );
+//           });
+//         }
+//         );
 
-      setLoading(false)  
-      toast.success("회원가입 완료되셨습니다")
-      navigate("/../src/pages/Login.jsx")
-    } catch (error) {
-      setLoading(false)
-      toast.error("잘못 입력하셨습니다");
-    }
-  };
+//       setLoading(false)  
+//       toast.success("회원가입 완료되셨습니다")
+//       navigate("/../src/pages/Login.jsx")
+//     } catch (error) {
+//       setLoading(false)
+//       toast.error("잘못 입력하셨습니다");
+//     }
+//   };
 
-  return(
-          <Helmet title="Signup">
-            <section>
-              <Container>
-                <Row>
-                 { 
-                 loading? <Col lg = '12' className="text-center">
-                  <h5 className="fw-bold">Loading....</h5></Col>
-                 : }
-                </Row>
-              </Container>
-            </section>
-          </Helmet>
-  ) 
-};
+//   return(
+//           <Helmet title="Signup">
+//             <section>
+//               <Container>
+//                 <Row>
+//                  { 
+//                  loading? <Col lg = '12' className="text-center">
+//                   <h5 className="fw-bold">Loading....</h5></Col>
+//                  : }
+//                 </Row>
+//               </Container>
+//             </section>
+//           </Helmet>
+//   ) 
+// };
 
-export default Signup;
+// export default Signup;
 
